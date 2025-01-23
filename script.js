46867531cc31d3174eec63579c5cef4ea7949342
Harumini234 <haruminishizawac@gmail.com>
2025-01-23 21:20:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("no-button").addEventListener("click", () => {
+  alert("Wrong answer! Try again. 😜");
+});
+
+document.getElementById("yes-button").addEventListener("click", () => {
+  alert("Yay! I knew you'd say yes! ❤️");
+});
+
